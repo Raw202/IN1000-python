2427509b4d3df70fbf19ded1b3ecff7ab93cb718
Rawan <78001037+Raw202@users.noreply.github.com>
2022-07-18 00:11:33 +0200

    Delete kodeforstaaelse.py

--- kodeforstaaelse.py
+++
@@ -1,8 +0,0 @@
-a = input("Tast inn et heltall! ")
-b = int(a)
-if b < 10:
-    print (b + "Hei!")
-#1-Dette er greit med variabel, input og if-test, men dette er
-#ikke lovlig kode fordi vi kan ikke slå sammen str og int. Det vil gi error
-#2-Vi får et problem for at vi ikke klarer å printe b som er int med ordet Hei
-#fordi man kan ikke printe et int og str på samme linje. 
